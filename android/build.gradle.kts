3a8bc0a4c92308a08d9c75ef01aeb0ebca396c35
sdzahag4 <sdzahag4@gmail.com>
2026-09-06 01:35:04 +0700

    Simplify root build.gradle.kts by removing incompatible afterEvaluate block

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -24,26 +24,6 @@ subprojects {
     project.evaluationDependsOn(":app")
 }
 
-// Blok pemaksaan SDK menggunakan Kotlin DSL yang kompatibel dengan AGP 9+
-subprojects {
-    afterEvaluate { p ->
-        p.plugins.withId("com.android.application") {
-            p.extensions.configure<com.android.build.api.dsl.ApplicationExtension> {
-                if (compileSdk < 36) {
-                    compileSdk = 36
-                }
-            }
-        }
-        p.plugins.withId("com.android.library") {
-            p.extensions.configure<com.android.build.api.dsl.LibraryExtension> {
-                if (compileSdk < 36) {
-                    compileSdk = 36
-                }
-            }
-        }
-    }
-}
-
 tasks.register<Delete>("clean") {
     delete(rootProject.layout.buildDirectory)
 }
